05c553c46617b006a0a26f62d66ea13ddcc432b0
RAGHAVRAWAT123 <raghavrawat290@gmail.com>
2024-04-01 21:34:43 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ee244335f9mshdb94fb823b75177p141893jsn3fccbf9a4841',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	},
+};
+
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY="01a1fb2703d4d3be1bd54d67232b7600";
